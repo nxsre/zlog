f1c90725559e3dabfd0fcc7a009a8f61f836c230
wangyj <wangyj@liepin.com>
2017-11-28 05:32:33 +0800

    init

---
+++ logger.go
@@ -0,0 +1,43 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package zlog
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// SetOutput replaces existing Core with new, that writes to passed WriteSyncer.
+func SetOutput(ws zapcore.WriteSyncer, conf zap.Config) zap.Option {
+	var enc zapcore.Encoder
+	// Copy paste from zap.Config.buildEncoder.
+	switch conf.Encoding {
+	case "json":
+		enc = zapcore.NewJSONEncoder(conf.EncoderConfig)
+	case "console":
+		enc = zapcore.NewConsoleEncoder(conf.EncoderConfig)
+	default:
+		panic("unknown encoding")
+	}
+	return zap.WrapCore(func(zapcore.Core) zapcore.Core {
+		return zapcore.NewCore(enc, ws, conf.Level)
+	})
+}

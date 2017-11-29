75b116c9b925cb9164c4ac38c2f9774e7ad5d4b8
wangyj <wangyj@liepin.com>
2017-11-29 16:31:12 +0800

    add strip ctrl code tools

---
+++ _example/tools_test_strap.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/soopsio/zlog/tools"
+)
+
+const src = "déjà vu" + // precomposed unicode
+	"\n\000\037 \041\176\177\200\377\n" + // various boundary cases
+	"as⃝df̅" // unicode combining characters
+
+func main() {
+	fmt.Println("source text:")
+	fmt.Println(src)
+	fmt.Println("\nas bytes, stripped of control codes:")
+	fmt.Println(tools.StripCtlFromBytes(src))
+	fmt.Println("\nas bytes, stripped of control codes and extended characters:")
+	fmt.Println(tools.StripCtlAndExtFromBytes(src))
+	fmt.Println("\nas UTF-8, stripped of control codes:")
+	fmt.Println(tools.StripCtlFromUTF8(src))
+	fmt.Println("\nas UTF-8, stripped of control codes and extended characters:")
+	fmt.Println(tools.StripCtlAndExtFromUTF8(src))
+	fmt.Println("\nas decomposed and stripped Unicode:")
+	fmt.Println(tools.StripCtlAndExtFromUnicode(src))
+}
